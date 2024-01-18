46f4224af3a18349a93bfee1a0b59a0404abd817
luchesigerman@gmail.com <luchesigerman@mail.com>
2024-01-18 19:30:15 -0300

    mejorando el carrito

--- carrito.js
+++ carrito.js
@@ -1,18 +1,27 @@
-const carrito = [];
-let producto;
-let opcion = confirm('¿Agregar Producto?');
+const transacciones = [
+    { concepto: 'Debito', saldo: 3000, fecha: '12-01-24'}
+]
+
+const carrito = [ 
+    { nombre: ' Lentes Toko', categoria: '1', precio: 5000},
+    { nombre: ' Lentes Marrok', categoria: '3', precio: 7500},
+    { nombre: ' Lentes Marzul', categoria: '3', precio: 50000},
+    { nombre: ' Lentes Rosal', categoria: '2', precio: 90000},
+    { nombre: ' Lentes Gernom', categoria: '2', precio: 75000},
+    { nombre: ' Lentes Vice', categoria: '1', precio: 75000},
+    { nombre: ' Lentes San Francisco', categoria: '1', precio: 75000},
+    { nombre: ' Lentes Roma', categoria: '4', precio: 75000},
+    { nombre: ' Lentes Aruba', categoria: '3', precio: 75000},
+    { nombre: ' Lentes Los Angeles', categoria: '5', precio: 75000},
+
+];
+
+carrito.push({ nombre: 'Web Cam', categoria: 'Computación', precio: 120000})
+
+console.table( carrito )
 
 
-while( opcion == true ){
-    producto = prompt('Producto ');
-    carrito.push( producto);
-
-    opcion = confirm('¿Agregar Producto?');
+for( let i=0; i<carrito.length; i++ ){
+    console.log('Nombre ' +  carrito[i].nombre );
+    console.log('Precio ' +  carrito[i].precio );    
 }
-
-// Muestro el carrito
-for(let i=0; i<carrito.length; i++ ){
-    console.log( carrito[i] );
-}
-console.log('🤯')
-console.table( carrito);
