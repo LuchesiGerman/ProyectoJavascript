aac844896d87c36c5c8bbc4ac1a4214277487778
luchesigerman@gmail.com <luchesigerman@mail.com>
2024-01-18 17:41:25 -0300

    iniciando proyecto

---
+++ claseo1.js
@@ -0,0 +1,45 @@
+/* ejepmlo 1=
+let usuario = prompt ( "ingrese el usuario");
+let rol = prompt ("ingrese el rol");
+let mensaje = "hola " + rol + " " + usuario;
+console.log(mensaje);
+alert ( mensaje); */
+
+
+
+let usuario = prompt('Ingrese el Usuario');
+usuario = usuario.toLowerCase();
+let password = prompt('Ingrese la contraseña');
+let mensaje;
+
+// Si el usuario es 'Ger' y el password es '123abc'
+//           true        Y           true  
+if(  usuario == 'Ger' || usuario =="ger"|| usuario =="GER" &&  password == '123abc' ) {
+    mensaje = 'Bienvenido ' + usuario;
+} else {
+    mensaje = 'Usuario o Contraseña Incorrectos';
+}
+
+alert(mensaje); 
+
+function calculoCarro(cantidad,precio,inputtext,totaltext){
+    let resultado = precio + cantidad;
+    // Calculo del subtotal
+    let subtotal = precio*cantidad;
+    let inputtextValue=subtotal;
+    //Calculo del total
+    let total = eval(totaltext.value);
+    totaltext.value = total + subtotal;
+    return resultado;
+  }
+
+console.log ("inicio del programa")
+let edad =26;
+ //declarando una funsion 
+
+function pedirNombre (){
+    let nombre = prompt("Ingrese el nombre");
+}
+
+pedirNombre();
+
